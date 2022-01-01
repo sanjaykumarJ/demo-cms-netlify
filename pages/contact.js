605a06d9f8cb0d321a42e7799b88cbd1a244036f
sanjay jawahar <balasanjaykumar@gmail.com>
2022-01-01 17:58:46 +0530

    Add contact us page

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,35 @@
 export default function Contact() {
-  return <>contact us</>;
+  return (
+    <div className="contact-form">
+      <h2>Contact us for your queries!</h2>
+      <form
+        action={`https://submit-form.com/${process.env.NEXT_PUBLIC_FORM_ID}`}
+      >
+        <label for="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          required=""
+        />
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          required=""
+        />
+        <label for="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Message"
+          required=""
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
